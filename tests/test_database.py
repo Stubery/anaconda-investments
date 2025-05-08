cab3fd6d781421b298869d17db25c9337b85af51
Stu <stuart.brande@gmail.com>
2025-05-08 12:23:51 +0800

    Add Database Setup Tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,10 +1,25 @@
+import os
+import pathlib
 import sqlite3
 import unittest
 
 class DatabaseTestCase(unittest.TestCase):
     def test_database_connection(self):
-        sqlite_connection = sqlite3.connect('..\\resources\\investments.db')
+
+        db_path = "resources/investments.db" if os.getcwd().endswith("anaconda-investments") else "../resources/investments.db"
+
+
+        print('CWD:     ', os.getcwd())
+        print('Absolute path of file:     ', db_path)
+
+        sqlite_connection = sqlite3.connect(db_path)
         cursor = sqlite_connection.cursor()
+
+        query = 'SELECT * FROM sqlite_master WHERE type="table";'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        print(f"Tables: {result}")
+
         query = 'SELECT * FROM equity_prices LIMIT 10;'
         cursor.execute(query)
         result = cursor.fetchall()
