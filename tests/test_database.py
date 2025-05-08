a03b934842676083a1e7d5c06b26b94ce9670fe3
Stu <stuart.brande@gmail.com>
2025-05-08 12:06:59 +0800

    Add Database Setup Tests

---
+++ tests/test_database.py
@@ -0,0 +1,14 @@
+import sqlite3
+import unittest
+
+class DatabaseTestCase(unittest.TestCase):
+    def test_database_connection(self):
+        sqlite_connection = sqlite3.connect('../resources/investments.db')
+        cursor = sqlite_connection.cursor()
+        query = 'SELECT * FROM equity_prices LIMIT 10;'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        assert len(result) == 10
+
+if __name__ == '__main__':
+    unittest.main()
